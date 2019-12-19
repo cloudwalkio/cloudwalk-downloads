6747a688a947ea7615079fe1427dbf97e486db73
Bruno Freitas <40842+bmfreitas@users.noreply.github.com>
2019-12-19 08:33:23 -0300

    Add proper redirect for not found paths

--- downloads.rb
+++ downloads.rb
@@ -1,14 +1,15 @@
 require 'rubygems'
 require 'sinatra'
 
+not_found do
+  status 301
+  redirect 'https://www.cloudwalk.io'
+end
+
 get '/:file' do |file|
   redirect "http://59ef8c7b3c1dfd2d7e71-64e65e4fc31f8f1083b5981dbfc9ea4a.r46.cf5.rackcdn.com/#{file}", 303
 end
 
 get '/:folder/:file' do |folder, file|
   redirect "http://59ef8c7b3c1dfd2d7e71-64e65e4fc31f8f1083b5981dbfc9ea4a.r46.cf5.rackcdn.com/#{folder}/#{file}", 303
-end
-
-get '*' do |file|
-  redirect "https://www.cloudwalk.io"
 end
