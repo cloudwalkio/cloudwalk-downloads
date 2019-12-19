d4c001e1ff5be49c080518846741893b76104fba
Bruno Freitas <40842+bmfreitas@users.noreply.github.com>
2019-12-19 08:36:43 -0300

    Move Rack CDN url to a helper method

--- downloads.rb
+++ downloads.rb
@@ -2,14 +2,19 @@ require 'rubygems'
 require 'sinatra'
 
 not_found do
-  status 301
-  redirect 'https://www.cloudwalk.io'
+  redirect 'https://www.cloudwalk.io', 301
 end
 
 get '/:file' do |file|
-  redirect "http://59ef8c7b3c1dfd2d7e71-64e65e4fc31f8f1083b5981dbfc9ea4a.r46.cf5.rackcdn.com/#{file}", 303
+  redirect "#{rackcdn_url}/#{file}", 303
 end
 
 get '/:folder/:file' do |folder, file|
-  redirect "http://59ef8c7b3c1dfd2d7e71-64e65e4fc31f8f1083b5981dbfc9ea4a.r46.cf5.rackcdn.com/#{folder}/#{file}", 303
+  redirect "#{rackcdn_url}/#{folder}/#{file}", 303
+end
+
+helpers do
+  def rackcdn_url
+    'http://59ef8c7b3c1dfd2d7e71-64e65e4fc31f8f1083b5981dbfc9ea4a.r46.cf5.rackcdn.com'
+  end
 end
